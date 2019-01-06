8c077dc4c6400a4d8de990eff6060a705e6b6041
draptis <dimitrisraptis96@gmail.com>
2019-01-06 23:09:21 +0200

    add typography and react-helmet

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,10 +2,18 @@ const path = require('path');
 
 module.exports = {
   siteMetadata: {
-    title: 'Dimitris Raptis | Portfolio',
+    author: 'Raptis Dimitris',
+    description: 'My personal portfolio website! ',
+    siteUrl: 'raptis.me',
+    title: 'Dimitris Raptis ',
+    twitter: 'https://twitter.com/d__raptis',
   },
   pathPrefix: "/personal-website",
   plugins: [
+    'gatsby-plugin-offline',
+    'gatsby-transformer-sharp',
+    'gatsby-plugin-sharp',
+    'gatsby-plugin-react-helmet',
     {
       resolve: `gatsby-source-filesystem`,
       options: {
@@ -13,8 +21,11 @@ module.exports = {
         path: path.join(__dirname, `src`, `images`),
       },
     },
-    'gatsby-plugin-offline',
-    'gatsby-transformer-sharp',
-    'gatsby-plugin-sharp'
+    {
+      resolve: `gatsby-plugin-typography`,
+      options: {
+        pathToConfigModule: `src/utils/typography`,
+      }
+    }
   ],
 }
