fa275bc74607343c199b40855157c0410113a69e
draptis <dimitrisraptis96@gmail.com>
2019-01-06 23:36:22 +0200

    add styled-component plugin

--- gatsby-config.js
+++ gatsby-config.js
@@ -14,6 +14,7 @@ module.exports = {
     'gatsby-transformer-sharp',
     'gatsby-plugin-sharp',
     'gatsby-plugin-react-helmet',
+    'gatsby-plugin-styled-components',
     {
       resolve: `gatsby-source-filesystem`,
       options: {
