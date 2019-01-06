7ef1ed58c12dc9a29f43bdfa2eb00610e5813d3d
draptis <dimitrisraptis96@gmail.com>
2019-01-06 23:10:51 +0200

    refactor folder structure

---
+++ src/components/Icons.js
@@ -0,0 +1,63 @@
+import React from 'react';
+import styled from 'styled-components';
+
+import { 
+  GitHub,
+  Linkedin,
+  Twitter,
+  Instagram,
+} from 'react-feather';
+
+import {
+  black,
+  green,
+} from '../utils/colors';
+
+const Wrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  padding: 1em 2em;
+
+  position: absolute;
+  bottom: 0;
+  right: 0;
+
+  > svg {
+    margin: 0.5em;
+  }
+`;
+
+const Anchor = styled.a`
+  padding: 10px;
+  
+  :hover {
+    > svg {
+      stroke: ${green};
+    }
+  }
+`;
+
+const Icons = ({}) => {
+  
+  return (
+    <Wrapper>
+      <Anchor href='https://github.com/dimitrisraptis96/'>
+        <GitHub color={black} size={24}/>
+      </Anchor>
+
+      <Anchor href='https://twitter.com/d__raptis'>
+        <Twitter color={black} size={24}/>
+      </Anchor>
+
+      <Anchor href='https://www.instagram.com/glitch_aholic/'>
+        <Linkedin color={black} size={24}/>
+      </Anchor>
+
+      <Anchor href='https://www.linkedin.com/in/dimitris-raptis-a7088b132/'>
+        <Instagram color={black} size={24}/>
+      </Anchor>
+    </Wrapper>    
+  );
+};
+
+export default Icons;
--- src/pages/index.js
+++ src/pages/index.js
@@ -3,25 +3,25 @@ import { graphql } from "gatsby"
 import Img from "gatsby-image"
 import styled from 'styled-components';
 
-import { GitHub } from 'react-feather';
-import { Linkedin } from 'react-feather';
-import { Twitter } from 'react-feather';
-import { Instagram } from 'react-feather';
+import Icons from '../components/Icons';
 
-const darkgreen = "#0D7B44";
-const green = "#33FF99";
-const black = "#121212";
-const white = "#f5f5f5";
+import {
+  darkgreen,
+  green,
+  black,
+  white
+} from '../utils/colors';
+
 
 const Content = styled.div`
   background-color: #f5f5f5;
   position: absolute;
   top: 0;
   left: 0;
-  min-width: calc(100% - 10%);
+  min-width: 100%;
+  max-width: 100%;
   min-height: 100%;
-  max-width: calc(100% - 10%);
-  /* max-height: 100%; */
+  max-height: 100%;
   margin: 0;
 
   padding: 0 5%;
@@ -34,55 +34,31 @@ const Content = styled.div`
   background-color: #ececec;
 `;
 
-const Typography = styled.p`
+const Paragraph = styled.p`
   font-family: 'Poppins', sans-serif;
-  font-size: 1.5em;
+  font-size: 1em;
   font-weight: regular;
   color: #121212;
+  text-align: center;
+  width: 100%;
+
+  padding: 2em;
 `;
 
-const Bold = styled.p`
-  @import url('https://fonts.googleapis.com/css?family=Abril+Fatface');
+const Header = styled.h2`
 	font-family: 'Abril Fatface', cursive;
   font-size: 5em;
-  /* font-weight: bold; */
+  font-weight: normal;
+  text-align: center;
   color: #121212;
 `;
 
-const IconWrapper = styled.div`
-  display: flex;
-  flex-direction: column;
-  /* align-self: flex-end;
-  justify-self: flex-end; */
-  padding: 1em 2em;
-
-  position: absolute;
-  bottom: 0;
-  right: 0;
-  
-  
-  > svg {
-    margin: 0.5em;
-  }
-`;
 
 const Image = styled(Img)`
   box-shadow: 0 8px 6px -6px black;
   border-radius: 1000px;
 `;
 
-const Anchor = styled.a`
-  padding: 10px;
-  -webkit-animation: icon3d 200ms 10;
-  animation: icon3d 200ms 10;
-  color: #fff;
-  
-  :hover {
-    > svg {
-      stroke: ${green};
-    }
-  }
-`;
 
 const Green = styled.span`
   color: ${green};
@@ -97,33 +73,17 @@ const Bio = styled.div`
 
 const Homepage = ({data}) => (
   <Content>
-    <Bold>
+    <Header>
       Dimitris Raptis <Green>.</Green>
-    </Bold>
+    </Header>
 
     <Image fixed={data.file.childImageSharp.fixed} />
 
-    <Typography >
+    <Paragraph >
       I design and build user interfaces
-    </Typography>
+    </Paragraph>
 
-    <IconWrapper>
-      <Anchor href='https://github.com/dimitrisraptis96/'>
-        <GitHub color={black} size={24}/>
-      </Anchor>
-
-      <Anchor href='https://twitter.com/d__raptis'>
-        <Twitter color={black} size={24}/>
-      </Anchor>
-
-      <Anchor href='https://www.instagram.com/glitch_aholic/'>
-        <Linkedin color={black} size={24}/>
-      </Anchor>
-
-      <Anchor href='https://www.linkedin.com/in/dimitris-raptis-a7088b132/'>
-        <Instagram color={black} size={24}/>
-      </Anchor>
-    </IconWrapper>
+    <Icons/>
 
   
   </Content>
