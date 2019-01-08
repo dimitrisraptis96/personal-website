db4e2268298a725bfdf87a75083fc32e1af4c99a
draptis <dimitrisraptis96@gmail.com>
2019-01-09 00:49:47 +0200

    change favicon vol.2

--- gatsby-config.js
+++ gatsby-config.js
@@ -22,7 +22,7 @@ module.exports = {
         background_color: `#663399`,
         theme_color: `#663399`,
         display: `minimal-ui`,
-        icon: `src/images/hipster.png`, // This path is relative to the root of the site.
+        icon: `src/images/munch.png`, // This path is relative to the root of the site.
       },
     },
     'gatsby-plugin-react-helmet',
