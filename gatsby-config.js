a6cc48064d8d3ac583fd33d41755a6185c52a13e
draptis <dimitrisraptis96@gmail.com>
2019-01-09 00:47:47 +0200

    change favicon

--- gatsby-config.js
+++ gatsby-config.js
@@ -5,14 +5,26 @@ module.exports = {
     author: 'Raptis Dimitris',
     description: 'My personal portfolio website! ',
     siteUrl: 'raptis.me',
-    title: 'Dimitris Raptis ',
+    title: 'Raptis Dimitris ',
     twitter: 'https://twitter.com/d__raptis',
   },
   pathPrefix: "/personal-website",
   plugins: [
     'gatsby-plugin-offline',
     'gatsby-transformer-sharp',
-    'gatsby-plugin-sharp',
+    `gatsby-plugin-sharp`,
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `gatsby-starter-default`,
+        short_name: `starter`,
+        start_url: `/`,
+        background_color: `#663399`,
+        theme_color: `#663399`,
+        display: `minimal-ui`,
+        icon: `src/images/hipster.png`, // This path is relative to the root of the site.
+      },
+    },
     'gatsby-plugin-react-helmet',
     'gatsby-plugin-styled-components',
     {
