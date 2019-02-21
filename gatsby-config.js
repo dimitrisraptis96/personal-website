86a9493b3375fd9662d31a0573799576a40ccd34
Dimitris <dimitrisraptis96@gmail.com>
2019-02-22 01:02:05 +0200

    update gatsby config

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,37 +3,75 @@ const path = require('path');
 module.exports = {
   siteMetadata: {
     author: 'Raptis Dimitris',
-    description: 'My personal portfolio website! ',
-    siteUrl: 'raptis.me',
-    title: 'Raptis Dimitris ',
-    twitter: 'https://twitter.com/d__raptis',
+    description: 'My god damn portfolio!',
+    siteUrl: 'raptis.wtf',
+    title: 'Portfolio | Raptis Dimitris ',
+    social: {
+      twitter: 'https://twitter.com/d__raptis',
+    }
   },
   pathPrefix: "/personal-website",
   plugins: [
     'gatsby-plugin-offline',
     'gatsby-transformer-sharp',
     `gatsby-plugin-sharp`,
+    'gatsby-plugin-react-helmet',
+    `gatsby-plugin-feed`,
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        path: `${__dirname}/content/blog`,
+        name: `blog`,
+      },
+    },
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        path: `${__dirname}/content/assets`,
+        name: `assets`,
+      },
+    },
+    {
+      resolve: `gatsby-transformer-remark`,
+      options: {
+        plugins: [
+          {
+            resolve: `gatsby-remark-images`,
+            options: {
+              maxWidth: 590,
+            },
+          },
+          {
+            resolve: `gatsby-remark-responsive-iframe`,
+            options: {
+              wrapperStyle: `margin-bottom: 1.0725rem`,
+            },
+          },
+          `gatsby-remark-prismjs`,
+          `gatsby-remark-copy-linked-files`,
+          `gatsby-remark-smartypants`,
+        ],
+      },
+    },
+    {
+      resolve: `gatsby-plugin-google-analytics`,
+      options: {
+        //trackingId: `ADD YOUR TRACKING ID HERE`,
+      },
+    },
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
         name: `gatsby-starter-default`,
-        short_name: `starter`,
+        short_name: `Dimitris Raptis`,
         start_url: `/`,
-        background_color: `#663399`,
-        theme_color: `#663399`,
+        background_color: `#f5f5f5`,
+        theme_color: `#33FF99`,
         display: `minimal-ui`,
-        icon: `src/images/munch.png`, // This path is relative to the root of the site.
+        icon: `${__dirname}/content/assets/munch.png`, // This path is relative to the root of the site.
       },
     },
-    'gatsby-plugin-react-helmet',
     'gatsby-plugin-styled-components',
-    {
-      resolve: `gatsby-source-filesystem`,
-      options: {
-        name: `images`,
-        path: path.join(__dirname, `src`, `images`),
-      },
-    },
     {
       resolve: `gatsby-plugin-typography`,
       options: {
