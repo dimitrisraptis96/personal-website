212f5f77455b8ec626225293c428bb1fcfb72965
Dimitris <dimitrisraptis96@gmail.com>
2019-02-22 01:04:58 +0200

    Update paths and typography

--- src/utils/typography.js
+++ src/utils/typography.js
@@ -23,4 +23,11 @@ const typography = new Typography({
   ],
 })
 
+// Hot reload typography in development.
+if (process.env.NODE_ENV !== `production`) {
+  typography.injectStyles()
+}
+
 export default typography
+export const rhythm = typography.rhythm
+export const scale = typography.scale
