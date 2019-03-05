3347ae89430770a5f0e3b5f56b33dadc3a4d7701
Dimitris <dimitrisraptis96@gmail.com>
2019-03-06 00:10:57 +0200

    Add dribbble link

--- src/components/Icons.js
+++ src/components/Icons.js
@@ -2,12 +2,12 @@ import React from 'react';
 import styled from 'styled-components';
 
 import { 
-  GitHub,
-  Linkedin,
-  Twitter,
-  Instagram,
-} from 'react-feather';
-
+  FiGithub as GitHub,
+  FiLinkedin as Linkedin,
+  FiTwitter as Twitter,
+  FiInstagram as Instagram,
+} from 'react-icons/fi';
+import {  FaDribbble as Dribbble } from 'react-icons/fa';
 import {
   green,
   gray,
@@ -33,6 +33,7 @@ const Anchor = styled.a`
   :hover {
     > svg {
       stroke: ${green};
+      fill: ${(props) => props.isFill ? green:''};
     }
   }
 `;
@@ -47,6 +48,10 @@ const Icons = ({}) => {
 
       <Anchor href='https://twitter.com/d__raptis'>
         <Twitter color={gray} size={24}/>
+      </Anchor>
+
+      <Anchor href='https://dribbble.com/dmraptis'>
+        <Dribbble isFill={true} color={gray} size={24}/>
       </Anchor>
 
       <Anchor href='https://www.linkedin.com/in/dimitris-raptis-a7088b132/'>
