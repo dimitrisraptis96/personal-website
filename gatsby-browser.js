8bf7899cc729f5366e774227007fb6b9028218ff
Dimitris Raptis <dimitrisraptis96@gmail.com>
2019-12-05 23:08:46 +0200

    :bug: Issues with plugins

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,2 +1,2 @@
-import './src/styles/custom-prism-theme.css'
-require("prismjs/plugins/line-numbers/prism-line-numbers.css")
+import "./src/styles/custom-prism-theme.css";
+require("prismjs/plugins/line-numbers/prism-line-numbers.css");
--- gatsby-config.js
+++ gatsby-config.js
@@ -1,36 +1,47 @@
-const path = require('path');
+const path = require("path");
 
 module.exports = {
   siteMetadata: {
-    author: 'Raptis Dimitris',
-    description: 'My god damn portfolio!',
-    siteUrl: 'raptis.wtf',
-    title: 'Raptis Dimitris',
-    blogTitle: 'Blog',
+    author: "Jim Raptis",
+    description: "The life of Raptis!",
+    siteUrl: "https://www.raptis.wtf",
+    title: "Raptis Jim",
+    blogTitle: "Blog",
     social: {
-      twitter: 'https://twitter.com/d__raptis',
+      twitter: "https://twitter.com/d__raptis"
     }
   },
   pathPrefix: "/personal-website",
   plugins: [
-    'gatsby-plugin-offline',
-    'gatsby-transformer-sharp',
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `Jim Raptis`,
+        short_name: `Jim Raptis portfolio`,
+        start_url: `/`,
+        background_color: `#f5f5f5`,
+        theme_color: `#33FF99`,
+        display: `minimal-ui`,
+        icon: `${__dirname}/content/assets/munch.png` // This path is relative to the root of the site.
+      }
+    },
+    "gatsby-transformer-sharp",
     `gatsby-plugin-sharp`,
-    'gatsby-plugin-react-helmet',
+    "gatsby-plugin-react-helmet",
     `gatsby-plugin-feed`,
     {
       resolve: `gatsby-source-filesystem`,
       options: {
         path: `${__dirname}/content/blog`,
-        name: `blog`,
-      },
+        name: `blog`
+      }
     },
     {
       resolve: `gatsby-source-filesystem`,
       options: {
         path: `${__dirname}/content/assets`,
-        name: `assets`,
-      },
+        name: `assets`
+      }
     },
     {
       resolve: `gatsby-transformer-remark`,
@@ -39,52 +50,42 @@ module.exports = {
           {
             resolve: `gatsby-remark-images`,
             options: {
-              maxWidth: 590,
-            },
+              maxWidth: 590
+            }
           },
           {
             resolve: `gatsby-remark-responsive-iframe`,
             options: {
-              wrapperStyle: `margin-bottom: 1.0725rem`,
-            },
+              wrapperStyle: `margin-bottom: 1.0725rem`
+            }
           },
           `gatsby-remark-prismjs`,
           `gatsby-remark-copy-linked-files`,
-          `gatsby-remark-smartypants`,
-        ],
-      },
+          `gatsby-remark-smartypants`
+        ]
+      }
     },
     {
       resolve: `gatsby-plugin-google-analytics`,
       options: {
-        trackingId: process.env.GOOGLE_ANALYTICS_ID,
-      },
+        trackingId: process.env.GOOGLE_ANALYTICS_ID
+      }
     },
     {
       resolve: `gatsby-plugin-hotjar`,
       options: {
         id: process.env.HOTJAR_ID,
         sv: process.env.HOTJAR_SNIPPET_VERSION
-      },
+      }
     },
-    {
-      resolve: `gatsby-plugin-manifest`,
-      options: {
-        name: `gatsby-starter-default`,
-        short_name: `Dimitris Raptis`,
-        start_url: `/`,
-        background_color: `#f5f5f5`,
-        theme_color: `#33FF99`,
-        display: `minimal-ui`,
-        icon: `${__dirname}/content/assets/munch.png`, // This path is relative to the root of the site.
-      },
-    },
-    'gatsby-plugin-styled-components',
+
+    "gatsby-plugin-styled-components",
     {
       resolve: `gatsby-plugin-typography`,
       options: {
-        pathToConfigModule: `src/utils/typography`,
+        pathToConfigModule: `src/utils/typography`
       }
     },
-  ],
-}
+    "gatsby-plugin-offline"
+  ]
+};
