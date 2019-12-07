e27619ad46d804b53740afe81b7d676feff4f0c5
Dimitris Raptis <dimitrisraptis96@gmail.com>
2019-12-08 00:00:35 +0200

    :lipstick: change branding color

--- src/components/Underline.js
+++ src/components/Underline.js
@@ -1,10 +1,10 @@
-import React from 'react';
-import styled from 'styled-components';
+import React from "react";
+import styled from "styled-components";
 
 import { green } from "../utils/colors";
 
 const Underline = styled.span`
-  background-image: linear-gradient(120deg, ${green}, ${green} 100%);
+  background-image: linear-gradient(120deg, ${green}44, ${green}44 100%);
   background-repeat: no-repeat;
   background-size: 100% 0.3em;
   background-position: 0 78%;
--- src/components/button.js
+++ src/components/button.js
@@ -1,24 +1,18 @@
-import React from 'react';
-import PropTypes from 'prop-types';
-import styled from 'styled-components';
+import React from "react";
+import PropTypes from "prop-types";
+import styled from "styled-components";
 
-import {
-  black,
-  green,
-} from '../utils/colors';
-import {
-  radius,
-  shadow,
-} from '../utils/theme';
+import { black, green } from "../utils/colors";
+import { radius, shadow } from "../utils/theme";
 
-const PADDING = '1em';
+const PADDING = "1em";
 
 const Wrapper = styled.a`
   color: inherit;
   padding: ${PADDING} 22px;
   position: relative;
   text-decoration: none;
-  transition: all .2s ease;
+  transition: all 0.2s ease;
 
   :before {
     box-shadow: ${shadow};
@@ -28,10 +22,10 @@ const Wrapper = styled.a`
     left: 0;
     display: block;
     border-radius: ${radius};
-    background: ${green};
-    width: calc(23px + 2*${PADDING});
-    height: calc(23px + 2*${PADDING});
-    transition: all .3s ease;
+    background: ${green}44;
+    width: calc(23px + 2 * ${PADDING});
+    height: calc(23px + 2 * ${PADDING});
+    transition: all 0.3s ease;
   }
 
   > span {
@@ -39,7 +33,7 @@ const Wrapper = styled.a`
     font-size: 16px;
     line-height: 18px;
     font-weight: 900;
-    letter-spacing: .25em;
+    letter-spacing: 0.25em;
     text-transform: capitalize;
     vertical-align: middle;
   }
@@ -54,30 +48,30 @@ const Wrapper = styled.a`
     stroke: ${black};
     stroke-width: 2;
     transform: translateX(-5px);
-    transition: all .3s ease;
+    transition: all 0.3s ease;
   }
 
   :hover {
+    color: white;
     :before {
       width: 100%;
       background: ${green};
     }
     svg {
-      transform: translateX(0);
-      }
+      stroke: white;
 
+      transform: translateX(0);
+    }
   }
-  
-  :active{
-    transform: scale(.96);
+
+  :active {
+    transform: scale(0.96);
   }
 `;
 
-const Button = (props) => {
-  const {
-    prop
-  } = props;
-  
+const Button = props => {
+  const { prop } = props;
+
   return (
     <Wrapper href="#">
       <span>{props.children}</span>
@@ -85,7 +79,7 @@ const Button = (props) => {
         <path d="M1,5 L11,5"></path>
         <polyline points="8 1 12 5 8 9"></polyline>
       </svg>
-    </Wrapper>  
+    </Wrapper>
   );
 };
 
